0662b754bbb9e14cf8d2302eb1336f3da6765d34
thinkpad <weisbrja@protonmail.com>
2021-03-23 23:37:24 +0100

    Small changes to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,32 +1,32 @@
 plugins {
-    application
-    id("org.openjfx.javafxplugin") version "0.0.9"
-    id("org.beryx.jlink") version "2.23.4"
+	application
+	id("org.openjfx.javafxplugin") version "0.0.9"
+	id("org.beryx.jlink") version "2.23.4"
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 application {
-    mainClass.set("de.weisbrja.App")
+	mainClass.set("de.weisbrja.App")
 }
 
 javafx {
 	version = "16"
-    modules("javafx.controls")
+	modules("javafx.controls")
 }
 
 jlink {
-    addOptions("--strip-debug", "--no-header-files", "--no-man-pages")
-    launcher {
-        name = "launch"
-    }
+	addOptions("--strip-debug", "--no-header-files", "--no-man-pages")
+	launcher {
+		name = "launch"
+	}
 }
 
 dependencies {
-    implementation("javax.vecmath", "vecmath", "1.5.2")
-    implementation("org.apache.commons", "commons-csv", "1.8")
+	implementation("javax.vecmath", "vecmath", "1.5.2")
+	implementation("org.apache.commons", "commons-csv", "1.8")
 }
 
 modularity.disableEffectiveArgumentsAdjustment()
